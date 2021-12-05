65c620ccacff307734f1b08b2e3d60432c1bbdc7
Zdravko Hristov <zdravkohristov0@gmail.com>
2021-12-05 10:44:59 +0200

    2021 day 5 2

--- 2021/5.js
+++ 2021/5.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { start } = require('repl');
 let input;
 
 try {
@@ -13,28 +14,40 @@ try {
 function solve(input) {
 	const bottom = [];
 	const lines = getLines(input);
-	const markPoint = getMarker(bottom);
 
 	lines.forEach(line => {
-		const { x1, y1, x2, y2 } = line;
-		let startPoint, endPoint, constantPoint, direction;
-
-		if (y1 === y2) {
-			startPoint = Math.min(x1, x2);
-			endPoint = Math.max(x1, x2);
-			constantPoint = y1;
-			direction = 'x';
-		} else {
-			startPoint = Math.min(y1, y2);
-			endPoint = Math.max(y1, y2);
-			constantPoint = x1;
-			direction = 'y';
-		}
-
-		markPoint(startPoint, endPoint, constantPoint, direction);
+		checkLine(line, bottom);
 	});
 
 	return countItems(bottom, value => value > 1);
+}
+
+function checkLine(line, bottom) {
+	const { x1, y1, x2, y2 } = line;
+
+	if (x1 !== x2 && y1 !== y2) {
+		const startY = Math.min(y1, y2);
+		const endY = Math.max(y1, y2);
+		const startX = startY === y1 ? x1 : x2;
+		const endX = endY === y1 ? x1 : x2;
+
+		diagonalMark(bottom, startX, startY, endX, endY);
+		return;
+	}
+
+	if (y1 === y2) {
+		startPoint = Math.min(x1, x2);
+		endPoint = Math.max(x1, x2);
+		constantPoint = y1;
+		direction = 'x';
+	} else if (x1 === x2) {
+		startPoint = Math.min(y1, y2);
+		endPoint = Math.max(y1, y2);
+		constantPoint = x1;
+		direction = 'y';
+	}
+
+	straightMark(bottom, startPoint, endPoint, constantPoint, direction);
 }
 
 function countItems(items, cb) {
@@ -49,26 +62,42 @@ function countItems(items, cb) {
 	return count;
 }
 
-function getMarker(board) {
-	return function (startPoint, endPoint, constantPoint, direction) {
-		if (direction === 'x') {
-			board[constantPoint] = board[constantPoint] || [];
+function diagonalMark(board, startX, startY, endX, endY) {
+	while (startY <= endY) {
+		board[startY] = board[startY] || [];
+		board[startY][startX] = (board[startY][startX] || 0) + 1;
+		startY++;
+		startX += startX > endX ? -1 : 1;
+	}
+}
 
-			for (let j = startPoint; j <= endPoint; j++) {
-				board[constantPoint][j] = (board[constantPoint][j] || 0) + 1;
-			}
+function straightMark(board, startPoint, endPoint, constantPoint, direction) {
+	if (direction === 'x') {
+		board[constantPoint] = board[constantPoint] || [];
 
-			return;
+		for (let j = startPoint; j <= endPoint; j++) {
+			board[constantPoint][j] = (board[constantPoint][j] || 0) + 1;
 		}
 
-		for (let i = startPoint; i <= endPoint; i++) {
-			board[i] = board[i] || [];
-			board[i][constantPoint] = (board[i][constantPoint] || 0) + 1;
-		}
-	};
+		return;
+	}
+
+	for (let i = startPoint; i <= endPoint; i++) {
+		board[i] = board[i] || [];
+		board[i][constantPoint] = (board[i][constantPoint] || 0) + 1;
+	}
+}
+
+function filterLine(line) {
+	const { x1, y1, x2, y2 } = line;
+	const xDiff = Math.abs(x1 - x2);
+	const yDiff = Math.abs(y1 - y2);
+
+	return x1 === x2 || y1 === y2 || xDiff === yDiff;
 }
 
 function getLines(input) {
+	console.log(5);
 	return input
 		.split('\n')
 		.map(line => {
@@ -78,7 +107,7 @@ function getLines(input) {
 
 			return { x1, y1, x2, y2 };
 		})
-		.filter(line => line.x1 === line.x2 || line.y1 === line.y2);
+		.filter(filterLine);
 }
 
 function obtainCoords(coords) {
